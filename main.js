5f8c6d43ef06e82c124c8fee79839536cd2b0dee
Guttemberg Meirelles Machado <guttemberg@gmail.com>
2020-09-02 18:24:52 -0300

    versão para homologacao

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Guttemberg Machado
+
 function _log (module, procedure, data){
 
     let time = new Date().toISOString().split('T')[1];
@@ -11,11 +13,53 @@ function _log (module, procedure, data){
     /*eslint-enable*/
 }
 
-function main(){
-     _log('main.js', 'main', 'start...');
-
-    _log('main.js', 'main', 'start...');
+function _bubbleSort(items) {
+    var length = items.length;
+    for (var i = 0; i < length; i++) {
+        for (var j = 0; j < (length - i - 1); j++) {
+            if(items[j].count < items[j+1].count) {
+                var tmp = items[j];
+                items[j] = items[j+1];
+                items[j+1] = tmp;
+            }
+        }
+    }
+    return items;
 }
 
-main()
+function main(){
+
+    _log('main.js', 'main', 'Início...');
+
+    let buffer = 'rihreeahrrkhrrrra';
+    let res = [];
+
+    _log('main.js', 'main', '   entrada: "' + buffer + '".');
+
+    for (let i = 0; i < buffer.length; i++ ) {
+        let char = buffer.substr(i,1);
+        let k;
+        for (k = 0; k < res.length; k++){
+            if(res[k].char == char){
+                res[k].count++;
+                _log('main.js', 'main', '   Incrementando "' + char + '" (count '  + res[k].count + ')');
+                break;
+            }
+        }
+
+        if (k >= res.length){
+            _log('main.js', 'main', '   Adicionando "' + char + '" (count: 1).');
+            res.push ({char: char, count: 1});
+        }
+
+    }
+
+    let sorted = _bubbleSort(res);
+
+    _log('main.js', 'main', '   O caractere "' + sorted[0].char + '" é o mais frequente, com ' +sorted[0].count + ' ocorrências.');
+
+    _log('main.js', 'main', 'fim');
+}
+
+main();
 
